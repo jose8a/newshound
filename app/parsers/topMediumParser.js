f12da5da75443151c69ce8d65fd69dbc61005c22
jose8a <ochovilla@gmail.com>
2017-12-20 16:41:11 -0800

    Add parsing for Top Medium stories

---
+++ app/parsers/topMediumParser.js
@@ -0,0 +1,48 @@
+let topMedium = "https://medium.com/browse/top";
+let request = require('request');
+let newsItemParser = require('./newsItemParser');
+let collectionParser = require('./siteParser')
+
+// 1. create topMedium-specific properties parser
+let topMediumPropertiesExtractor = {
+  extractTitle: function(itemContainer, linkItem) {
+    return linkItem.find("h3.graf--title").text();
+  },
+  extractUrl: function(itemContainer, linkItem) {
+    return linkItem.attr("href").split('?')[0];
+  },
+  extractId: function(itemContainer, linkItem) {
+    return itemContainer.data("post-id");
+  },
+  extractRank: function(itemContainer, linkItem, rank) {
+    return rank;
+  },
+}
+
+// 2. create a topMediumItemParser from the generic newsItemParser in
+//    order to assign topMedium-specific properties to it
+let topMediumItemParser = Object.create(newsItemParser);
+topMediumItemParser.init(".postArticle", ".postArticle-content > a", "top-medium");
+
+// 3. merge both objects into the topMediumItemParser object using
+//    Object.assign(..)
+topMediumItemParser = Object.assign(topMediumItemParser, topMediumPropertiesExtractor)
+
+// 4. create a topMediumSiteParser to handle and orchestrate the actual parsing
+//    on the site
+let topMediumSiteParser = Object.create(collectionParser);
+
+// 5. use the parser in conjunction with cheerio and request/request to
+//    retrieve stories from the site
+let fetchTopMediumStories = function(req, res) {
+  request(topMedium, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      topMediumSiteParser.init(html, topMediumItemParser);
+      topMediumSiteParser.parseCollection();
+
+      res.status(200).json(topMediumSiteParser.getParsedItems());
+    }
+  });
+};
+
+module.exports = fetchTopMediumStories;
