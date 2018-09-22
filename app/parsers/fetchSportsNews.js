3230599f269327166103cc0e791f78a3f773c0df
jose8a <ochovilla@gmail.com>
2018-09-22 07:14:40 -0700

    Update the aggregator fetchSportsNews file to use the updated and DRYer sports-site parsers.

--- app/parsers/fetchSportsNews.js
+++ app/parsers/fetchSportsNews.js
@@ -1,17 +1,19 @@
-let fetchRingerStories = require("../parsers/ringerParser");
-let fetchSIStories = require("../parsers/sicomParser");
-let fetchSBNationStories = require("../parsers/sbnationParser");
-let fetchSBNCardinalStories = require("../parsers/sbnCardParser");
+let fetchSiteItems = require("./parserActions");
+
+let ringerConfigs = require("./configs/ringerParserConfig");
+let siConfigs = require("./configs/sicomParserConfig");
+let sbNationConfigs = require("./configs/sbnationParserConfig");
+let cardNationConfigs = require("./configs/sbnCardParserConfig");
 
 module.exports = async function() {
   let results = [];
   let response = [];
 
   results = await Promise.all([
-    fetchRingerStories(),
-    fetchSIStories(),
-    fetchSBNationStories(),
-    fetchSBNCardinalStories()
+    fetchSiteItems(ringerConfigs),
+    fetchSiteItems(siConfigs),
+    fetchSiteItems(sbNationConfigs),
+    // fetchSiteItems(cardNationConfigs)
   ]);
 
   results.forEach((list) => {
